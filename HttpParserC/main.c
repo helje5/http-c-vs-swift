c0260da17b0634250d02715c7b085fa1f2976075
Helge Hess <me@helgehess.eu>
2016-06-06 22:24:13 +0200

    Setting the stage for the competition.

---
+++ HttpParserC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HttpParserC
+//
+//  Created by Helge Heß on 06/06/16.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+    return 0;
+}
---
+++ HttpParserSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HttpParserSwift
+//
+//  Created by Helge Heß on 06/06/16.
+//
+//
+
+import Foundation
+
+print("Hello, World!")
+
